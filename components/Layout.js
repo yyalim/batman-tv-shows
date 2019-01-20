51af699c4bad93c942ee3214fdfb8855f2c89c17
Yusuf Yalim <yusufyalim@gmail.com>
2019-01-20 16:56:32 +0300

    Add normalize pixels for mobile

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ import Header from './Header'
 const Layout = ({ children }) => (
   <Fragment>
     <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <title>Batman TV Shows</title>
     </Head>
     <Header/>
