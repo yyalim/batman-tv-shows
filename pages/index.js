af378b626bfc90f6b0f866cb3619a9261d23f2b0
Yusuf Yalim <yusufyalim@gmail.com>
2019-01-20 14:52:09 +0300

    Compose show link list

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,13 @@ import Layout from '../components/Layout'
 import { withRouter } from 'next/router'
 import { Link } from '../routes'
 
-const ShowLinkList = ({ show }) => (
+const DetailsLinkList = ({ children }) => (
+  <ul>
+    {children}
+  </ul>
+)
+
+const DetailsLink = ({ show }) => (
   <li>
     <Link route="details" params={{ id: show.id }}>
       <a>{show.name}</a>
@@ -15,9 +21,9 @@ const ShowLinkList = ({ show }) => (
 const Index = ({ showIds, shows }) => (
   <Layout>
     <h1>BATMAN TV SHOWS</h1>
-    <ul>
-      {showIds.map(id => <ShowLinkList key={id} show={shows[id].show}/>)}
-    </ul>
+    <DetailsLinkList>
+      {showIds.map(id => <DetailsLink key={id} show={shows[id].show}/>)}
+    </DetailsLinkList>
   </Layout>
 )
 
