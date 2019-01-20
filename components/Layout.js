7aed9dbfa883d070482c6e76005e3e2f39128312
Yusuf Yalim <yusufyalim@gmail.com>
2019-01-20 16:26:17 +0300

    Add title

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
 import { Fragment } from 'react'
+import Head from 'next/head'
 import Header from './Header'
 
 const Layout = ({ children }) => (
   <Fragment>
+    <Head>
+      <title>Batman TV Shows</title>
+    </Head>
     <Header/>
     <main>
       {children}
